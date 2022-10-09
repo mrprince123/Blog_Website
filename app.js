89d58b41cca354477f156722b3dc0ee3a25cfde2
Prince <princekrdss2018@gmail.com>
2022-10-09 23:11:27 +0530

    Done Some port change

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ app.post("/login", function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 3000;
 }
 
 app.listen(port, function () {
