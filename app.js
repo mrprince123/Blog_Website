ecbe6c94e74c8d922f363eb1375f8782a0ff72c6
Prince <princekrdss2018@gmail.com>
2022-11-09 00:01:11 +0530

    Done Some change in the app.js file

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://blog-website-public.herokuapp.com/auth/google/main",
+    callbackURL: "https://blog-website.up.railway.app/auth/google/main",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
